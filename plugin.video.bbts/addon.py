3be23c7c5dfb1eb19ad91ea70f2f4f0784a4e35b
BBTS- <bbts.iptv@gmail.com>
2015-01-14 02:53:12 +0200

    new hub

---
+++ plugin.video.bbts/addon.py
@@ -0,0 +1,67 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
+import urllib
+import urllib2
+import sys
+import re
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
+import xbmc
+import os
+import generator
+
+def unwrapValue(entry, key, default):
+    v = None
+    if key in entry:
+        v = entry[key]
+    if v == None:
+        return default
+    return v
+
+def addVideoItem(name, url, iconimage, description):
+    addon_handle = int(sys.argv[1])
+    item = xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
+    item.setInfo(type='Video', infoLabels={'Title': name, 'Plot': description})
+    item.setProperty('IsPlayable', 'true')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=item, isFolder=False)
+
+
+def showFolderEntries(entries):
+    for e in entries:
+        title = unwrapValue(e,'title', 'Unknown')
+        url = unwrapValue(e,'url', '')
+        icon = unwrapValue(e,'tvg-logo', 'DefaultVideo.png')
+        addVideoItem(title.strip(), url, icon, title)
+
+
+def addFolders(folders):
+    for f in folders:
+        if f['icon'] == '':
+            icon = 'DefaultFolder.png'
+        else:
+            icon = f['icon']
+        url = sys.argv[0] + '?folder=' + urllib.quote_plus(f['name'])
+        li = xbmcgui.ListItem(f['name'], iconImage=icon)
+        li.setProperty('IsPlayable', 'false')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+
+addon_handle = int(sys.argv[1])
+xbmcplugin.setContent(addon_handle, 'movies')
+
+# xbmcgui.Dialog().ok("DEBUG", "[" + sys.argv[2] + "]")
+
+entries = generator.parseList()
+
+if "?folder=" in sys.argv[2]:
+    name = sys.argv[2].split("=")[1]
+    entries = generator.entriesInFolder(entries,urllib.unquote(name))
+    #entries.sort(key=lambda x: x['title'])
+    showFolderEntries(entries)
+else:
+    folders = generator.getFolders(entries)
+    #folders.sort()
+    addFolders(folders)
+xbmcplugin.endOfDirectory(addon_handle)
